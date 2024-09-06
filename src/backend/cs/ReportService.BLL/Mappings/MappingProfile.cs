9384a3953f1d4f2dd6331398686bc55193690fb8
Галиев Нияз Рафисович <galievnr@gmail.com>
2024-09-06 08:47:13 +0500

    Добавлен метод Save

--- src/backend/cs/ReportService.BLL/Mappings/MappingProfile.cs
+++ src/backend/cs/ReportService.BLL/Mappings/MappingProfile.cs
@@ -12,7 +12,9 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<CommitModel, CommitEntity>();
+        CreateMap<CommitModel, CommitEntity>()
+            .ForMember(dest => dest.ProcessedImage, opt => opt.MapFrom(src => Convert2(src.ProcessedImage)
+            ));;
         CreateMap<CommitEntity, CommitModel>()
             .ForMember(dest => dest.Defects, opt => opt.MapFrom(src => new List<DefectModel>()))
             .ForMember(dest => dest.StructuralElements,
@@ -30,5 +32,23 @@ public class MappingProfile : Profile
         
         CreateMap<ReportModel, ReportEntity>();
         CreateMap<ReportEntity, ReportModel>();
+    }
+    
+    public byte[] Convert2(string s)
+    {
+        // Преобразование строки Base64 в массив байтов
+        if (string.IsNullOrEmpty(s))
+        {
+            return null;
+        }
+        try
+        {
+            return Convert.FromBase64String(s);
+        }
+        catch (FormatException)
+        {
+            // Обработка ошибки в случае некорректной строки Base64
+            return null;
+        }
     }
 }
